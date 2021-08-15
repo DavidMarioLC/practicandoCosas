e187a6c1579dcfb4053ce9ce47d4de72dc1f0a5d
DavidMarioLC <davidmariolc.dev@gmail.com>
2021-08-15 16:08:22 -0500

    add changes

---
+++ infinityScroll/app.js
@@ -0,0 +1,38 @@
+const container = document.querySelector(".container");
+console.log(window);
+const URL = "https://dog.ceo/api/breeds/image/random";
+
+// get the images
+
+function loadImages(numImages = 10) {
+  let i = 0;
+  while (i < numImages) {
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then((response) => response.json())
+      .then((data) => {
+        // console.log(data.message)
+        const img = document.createElement("img");
+        img.src = `${data.message}`;
+        container.appendChild(img);
+      });
+    i++;
+  }
+}
+
+loadImages();
+
+// listen for scroll event and load more images if we reach the bottom of window
+window.addEventListener("scroll", () => {
+  console.log(window);
+  console.log("SCROLL Y: ", window.scrollY);
+  console.log("INNER HEIGHT: ", window.innerHeight);
+  console.log("SCROLL HEIGHT: ", document.body.scrollHeight);
+  // console.log("scrolled Y", window.scrollY); //scrolled from top
+  // console.log("InnerHeight", window.innerHeight); //visible part of screen
+  // if (
+  //   window.scrollY + window.innerHeight >=
+  //   document.documentElement.scrollHeight
+  // ) {
+  //   loadImages();
+  // }
+});
